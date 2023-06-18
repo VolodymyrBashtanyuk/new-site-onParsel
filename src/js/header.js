5b526569891d2c72047cc41b46284004be3d02bb
Volodymyr Bashtanyuk <volodymyrbashtanyuk@MacBook-Air-Volodymyr.local>
2023-06-18 22:42:35 +0100

    add icon in header

--- src/js/header.js
+++ src/js/header.js
@@ -1,20 +1,25 @@
 const button = document.querySelector('.header__btn');
 
 
-
-const mobileButton = () => {
-  const currentValue = button.textContent;
-    if (currentValue === 'ON') {
-        button.textContent = 'OFF';
-    } else {
-        button.textContent = 'ON';
-        
+// 
+const mobileButton = (event) => {
+    // console.log(event.currentTarget)
+//   const currentValue = button.textContent;
+    // if (currentValue === 'ON') {
+        // button.textContent = 'OFF';
+    // } else {
+        // button.textContent = 'ON';
+        // 
+// }
 }
-}
 
 
 
-button.addEventListener('click', mobileButton);
+button.addEventListener('click', (e) => {
+    let iconElement = e.currentTarget.childNodes[1].childNodes[1];
+   iconElement.getAttribute('href') === '/mobile.676bdaab.svg#icon-menu' ? iconElement.setAttribute('href', '/mobile.676bdaab.svg#icon-close') : iconElement.setAttribute('href', '/mobile.676bdaab.svg#icon-menu'); 
+   
+});
 
 export default mobileButton;
 
